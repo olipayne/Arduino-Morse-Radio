ba2062cbea09fe6de8f8c07defa3f976fabdbe40
Oliver Payne <olipayne@gmail.com>
2025-09-21 18:18:57 +0200

    meh

--- include/Version.h
+++ include/Version.h
@@ -2,6 +2,6 @@
 #define VERSION_H
 
 // Automatically generated from Git - do not edit manually
-#define FIRMWARE_VERSION "v1.12.2-dev-main-271f722b-dirty"
+#define FIRMWARE_VERSION "v1.12.1-dev-main-271f722b-dirty"
 
-#endif // VERSION_H
+#endif  // VERSION_H
