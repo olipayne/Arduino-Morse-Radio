ef13894288760294ac3140541c2a499b5bbd7331
Oliver Payne <olipayne@gmail.com>
2025-11-09 23:17:02 +0100

    chore: bump version to v1.16.0

--- include/Version.h
+++ include/Version.h
@@ -2,6 +2,6 @@
 #define VERSION_H
 
 // Automatically generated from Git - do not edit manually
-#define FIRMWARE_VERSION "v1.15.0-dev-main-766a1e5f-dirty"
+#define FIRMWARE_VERSION "v1.16.0"
 
 #endif // VERSION_H
